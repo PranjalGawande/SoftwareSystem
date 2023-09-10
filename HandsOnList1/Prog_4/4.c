79e8720ec95c3a7a890d145522737612e18c5f6a
PranjalGawande <gawandepranjal.pg@gmail.com>
2023-09-10 19:51:24 +0530

    Fixed Error

--- HandsOnList1/Prog_4/4.c
+++ HandsOnList1/Prog_4/4.c
@@ -1,3 +1,14 @@
+/*
+=============================================================================================
+Name : 4.c
+Author : Pranjal Gawande
+Description : Write a program to open an existing file with read write mode. 
+              Try O_EXCL flag also.
+Date: 17th Aug, 2023.
+=============================================================================================
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,7 +17,7 @@
 
 int main() {
 	const char* filename = "my_file2.txt";
-	int fd = open(filename, O_RDWR | O_EXCL);
+	int fd = open(filename, O_CREAT | O_EXCL, 0766);
 
 	if (fd == -1) {
 		if (errno == EEXIST) {
