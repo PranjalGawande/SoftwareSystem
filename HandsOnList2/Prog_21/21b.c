23db473e74b6cb07d0ccdba7eab49af9894e3e74
Pranjal Gawande <62420087+PranjalGawande@users.noreply.github.com>
2023-10-09 19:46:14 +0530

    two way communications

---
+++ HandsOnList2/Prog_21/21b.c
@@ -0,0 +1,64 @@
+/*
+============================================================================
+Name : 21b.c
+Author : Pranjal Gawande
+Description : Write two programs so that both can communicate by FIFO-
+              Use two way communication.
+Date: 5 Oct, 2023.
+============================================================================
+*/
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main()
+{
+    int fd;
+    char * myfifo = "fifoFile";
+    char buf[1024];
+
+    while (1)
+    {        
+        fd = open(myfifo,O_RDONLY);
+        if(fd == -1){
+            perror("Error in opening fifo file");
+            return 1;
+        }
+        
+        ssize_t byteread = read(fd, buf, sizeof(buf));
+        if(byteread == -1) {
+            perror("Error in reading from file");
+            return 1;
+        }
+        
+        printf("Program 1 : %s\n", buf);
+        close(fd);
+
+
+        fd = open(myfifo, O_WRONLY);
+        if(fd == -1){
+            perror("Error in opening fifo file");
+            return 1;
+        }         
+        
+        printf("Enter a message or type 'exit' to close connection: ");
+        fgets(buf, 1024, stdin);
+        
+        if(strcmp(buf,"exit\n") == 0) {    
+            unlink(myfifo);
+            close(fd);
+            exit(0);
+        }
+        
+        write(fd, buf, sizeof(buf));
+        close(fd);
+        
+    }
+    return 0;
+}
